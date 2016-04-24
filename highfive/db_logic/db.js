0fc939bbdcc30f93811f12a23effbd0d5cc82308
Steffen Christensen <mail@steffenc.me>
2016-04-24 04:30:14 -0400

    Added GET/POST for users + products, and added check for dummy API key

--- highfive/db_logic/db.js
+++ highfive/db_logic/db.js
@@ -11,27 +11,55 @@ var itemsSchema = new mongoose.Schema({
   endDate: String,
   location: String,
   owner_id: Number
-
 })
 
 var itemModel = mongoose.model("items", itemsSchema);
 //module.exports = itemModel;
 
+var usersSchema = new mongoose.Schema({
+  id: Number,
+  firstName: String,
+  lastName: String,
+  email: String,
+  location: String,
+  pwdHash: String,
+  AuthType: String
+})
+
+var usersModel = mongoose.model("users", usersSchema);
+//module.exports = itemModel;
+
+
+
 exports.findItems = function(callback){
-  console.log("2");
   itemModel.find(function (error, items) {
     var itemsList = [];
-
     items.forEach(function(i) {
-      itemsList.push({"title": i.title, "description": i.description});
+      itemsList.push({"title": i.title, "description": i.description, "price": i.price, "picture": i.picture_path, "owner_er": i.owner_id});
     });
     callback(itemsList);
   });
 }
 
-function saveItems(item) {
+exports.saveItem = function(item, callback) {
    new itemModel(item).save(function(e){
-     return 200;
+     console.log(e);
+     callback(200);
    });
+}
 
+exports.findUsers = function(callback) {
+  usersModel.find(function(error, users) {
+    var usersList = [];
+    users.forEach(function(i) {
+      usersList.push({"id": i.id, "firstname": i.firstName, "lastname": i.lastName, "email": i.email, "location": i.location});
+    });
+    callback(usersList);
+  });
+}
+
+exports.saveUser = function(user, callback) {
+   new usersModel(user).save(function(e){
+     callback(200);
+   });
 }
--- highfive/routes/index.js
+++ highfive/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var db_functions = require('../db_logic/db.js')
+var db_functions = require('../db_logic/db.js');
+var auth = require('./auth.js');
+var status = require('./errorHandling.js');
 
 
 /* GET home page. */
@@ -8,19 +10,36 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+* PRODUCTS
+**/
 
+/* Return all products */
 router.get('/products/', function(req, res, next) {
-  console.log("1");
   db_functions.findItems(function(itemsList){
-    console.log(itemsList);
     res.json(itemsList);
   });
 });
 
+/* Persist new product */
 router.post('/products/', function(req, res) {
-  var item = req;
-  var status = saveItems(item);
-  res.send(status);
+  auth.post(res, req, status.denied, db_functions.saveItems) ;
+});
+
+/**
+* USERS
+**/
+
+/* Return all users */
+router.get('/users/', function(req, res, next) {
+  db_functions.findUsers(function(usersList){
+    res.json(usersList);
+  });
+});
+
+/* Persist new user */
+router.post('/users/', function(req, res) {
+  auth.post(res, req, status.denied, db_functions.saveUser) ;
 });
 
 module.exports = router;
